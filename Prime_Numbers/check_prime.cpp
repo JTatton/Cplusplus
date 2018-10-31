d96584cb41839e033c70356623d12fdaba66e6c4
JTatton <7052428+JTatton@users.noreply.github.com>
2018-10-31 15:00:39 +1030

    Improve Complexity of check prime

--- Prime_Numbers/check_prime.cpp
+++ Prime_Numbers/check_prime.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
+#include <math.h>
+
 using namespace std;
 int main(){
-    int number,i;
-    cout<<"Enter the number ";
+    int number,i, sqrtNumber;
+    cout<<"Enter the number: ";
     cin>>number;
 
-    for(i=2;i<=(number/2);i++)
+    sqrtNumber = sqrt(number);
+
+    for(i=2;i<=sqrtNumber;i++)  // Only need to check up to sqrt of number
         if((number%i)==0)
             break; // cout<<number<<" is not Prime Number";
 
     if(--i==(number/2))                       // as value of i after for loop will be 1+
-        cout<<number<<" is Prime Number";
+        cout<<number<<" is a Prime Number" << endl;
     else                                      
-        cout<<number<<" is not Prime Number";
+        cout<<number<<" is not a Prime Number" << endl;
     return 0;
 }
